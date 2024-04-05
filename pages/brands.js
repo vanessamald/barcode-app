08abf0a71f053c8f26e54dbd84ad729f50e997c4
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2024-04-05 11:18:37 -0500

    updating packages

---
+++ pages/brands.js
@@ -0,0 +1,42 @@
+//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
+function Brands() {
+    const [brandData, setBrandData ] = useState([]);
+
+    // Fetch brand data when component mounts
+    useEffect(()=> {
+      fetchBrandData();
+    }, []);
+
+    const fetchBrandData = async () => {
+    try {
+      // Make a GET request to your backend API endpoint to fetch brand data
+      const response = await fetch(`/api/brands`);
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Brand Data:', data);
+        
+        setBrandData(data);
+      } else {
+        throw new Error('Failed to fetch brand information');
+      }
+    } catch (error) {
+      console.error('Error fetching brand information:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Brands</h1>
+      <ul>
+        {brandData.map((brand, index) => (
+          <li key={index}>{brand.name}</li>
+        ))} 
+      </ul>
+    </div>
+  )
+}
+
+export default Brands;
